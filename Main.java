3ad0948c028983cddb1cb46ddc9efbe767833d8b
TheWhiteTorreta <torreta.markdwight@gmail.com>
2023-12-09 23:04:15 +0800

    On the process of creating the Login GUI

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 package hatod;
 
+import hatod.controller.*;
+
 public class Main{
 
     public static void main(String[] args){
 
-
+        new LoginController();
 
     }
 
--- controller/LoginController.java
+++ controller/LoginController.java
@@ -1,14 +1,32 @@
 package hatod.controller;
 
+
+import javax.imageio.*;
+import java.io.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class LoginController{
 
+    Image backgroundImage;
+    JTextField email;
+    JTextField name;
+    JTextField phone;
+    JPasswordField password;
+    JTextField motorPlate;
+
     public LoginController(){
 
+        backgroundImage = Toolkit.getDefaultToolkit().createImage("LoginFrame.png");
 
+        
+
+
+    }
+
+    public void paint(Graphics graphic){
+        graphic.drawImage(backgroundImage, 0, 0, null);
     }
 
 }
