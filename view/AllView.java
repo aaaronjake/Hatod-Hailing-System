5786b6d503380b4b91b1719c46462dbd121c1ed0
Aaron Jake Alfante <151142188+aaaronjake@users.noreply.github.com>
2023-12-09 20:31:22 +0800

    Update AllView.java

--- view/AllView.java
+++ view/AllView.java
@@ -1,9 +1,40 @@
-package hatod.view;
+package grade.sheet.controller;
 
+import javax.swing.*;
+import java.awt.*;
 
+class AllView{
 
-public class AllView{
+	AllView(){
+		
 
-    
+	}
+	// public static void addpanel()
+	public static void addlabel(JLabel label, JPanel panel, String text, int x, int y, int width, int height, int fontSize){
+		label.setBounds(x,y,width,height);
+		label.setText(text);
+		label.setFont(new Font("Times New Roman", Font.BOLD, fontSize));
+        label.setForeground(new Color(0xC9E7EB));
+		panel.add(label);
+	}
+
+	public static void addtextField(JTextField textField, JPanel panel, int x, int y, int width, int height, int fontSize){
+		textField.setBounds(x,y,width,height);
+		textField.setFont(new Font("Times New Roman", Font.PLAIN, fontSize));
+		panel.add(textField);
+	}
+
+	public static void addpassField(JPasswordField passField, JPanel panel, int x, int y, int width, int height, int fontSize){
+		passField.setBounds(x,y,width,height);
+		passField.setFont(new Font("Times New Roman", Font.PLAIN, fontSize));
+		panel.add(passField);
+	}
+
+	public static void addbutton(JButton button, JPanel panel, String text, int x, int y, int width, int height, int fontSize){
+		button.setBounds(x,y,width,height);
+		button.setText(text);
+		button.setFont(new Font("Times New Roman", Font.PLAIN, fontSize));
+		panel.add(button);
+	}
 
 }
