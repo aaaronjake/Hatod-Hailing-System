faad637ced2fe5e5cd08889c2b5fe6ac77c6690b
Aaron Jake Alfante <151142188+aaaronjake@users.noreply.github.com>
2023-12-09 20:31:43 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,15 @@
 package hatod;
 
-public class Main{
+import javax.swing.*;
+class Main{
+	public static void main(String[] args){
+		JFrame frame = new JFrame();
+		frame.setSize(500,500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
 
-    public static void main(String[] args){
-
-
-
-    }
-
+		new LoginController(frame);
+	}
 }
