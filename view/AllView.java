9e147c6cd698cf7c707979e98077b8640c7c38c7
Aaron Jake Alfante <151142188+aaaronjake@users.noreply.github.com>
2023-12-10 14:39:54 +0800

    Update AllView.java

--- view/AllView.java
+++ view/AllView.java
@@ -5,47 +5,54 @@ import java.awt.*;
 
 public class AllView{
 
-    public static void addPanel(JPanel panel, JFrame frame, int x, int y, int width, int height){
-        panel.setBounds(x, y, width, height);
+	public AllView(){
+		
+
+	}
+	public static void addPanel(JFrame frame, JPanel panel){
+		panel.setBounds(0,0,500,500);
+        panel.setLayout(null);
+        panel.setOpaque(false);
         frame.add(panel);
-    }
-
-    public static void addImage(ImageIcon image, JPanel panel, int x, int y){
-        JLabel label = new JLabel(img);
-      	label.setBounds(x, y, img.getIconWidth(), img.getIconHeight());
-      	panel.add(label);
-    }
-
-	public static void addlabel(JLabel label, JPanel panel, String text, int x, int y, int width, int height, int fontSize){
+	}
+	public static void addLabel(JLabel label, JPanel panel, String text, int x, int y, int width, int height, int fontSize){
 		label.setBounds(x,y,width,height);
 		label.setText(text);
 		label.setFont(new Font("Times New Roman", Font.BOLD, fontSize));
+        label.setForeground(new Color(0xC9E7EB));
 		panel.add(label);
 	}
 
-	public static void addtextField(JTextField textField, JPanel panel, int x, int y, int width, int height, int fontSize){
+	public static void addImage(JFrame frame, JLabel label){
+		ImageIcon image = new ImageIcon("LoginFrame.png");
+		label.setBounds(0,-30,500,500);
+		label.setIcon(image);
+		frame.add(label);
+	}
+
+	public static void addTextField(JTextField textField, JPanel panel, int x, int y, int width, int height, int fontSize){
 		textField.setBounds(x,y,width,height);
 		textField.setFont(new Font("Times New Roman", Font.PLAIN, fontSize));
 		panel.add(textField);
 	}
 
-	public static void addpassField(JPasswordField passField, JPanel panel, int x, int y, int width, int height, int fontSize){
+	public static void addPassField(JPasswordField passField, JPanel panel, int x, int y, int width, int height, int fontSize){
 		passField.setBounds(x,y,width,height);
 		passField.setFont(new Font("Times New Roman", Font.PLAIN, fontSize));
 		panel.add(passField);
 	}
 
-	public static void addbutton(JButton button, JPanel panel, String text, int x, int y, int width, int height, int fontSize){
+	public static void addButton(JButton button, JPanel panel, String text, int x, int y, int width, int height, int fontSize){
 		button.setBounds(x,y,width,height);
 		button.setText(text);
 		button.setFont(new Font("Times New Roman", Font.PLAIN, fontSize));
 		panel.add(button);
 	}
-
-	public static void clearFrame(JFrame frame){
-      frame.getContentPane().removeAll();
-      frame.repaint();
-      frame.validate();
-    }
+	public static void addButton(JButton button, String text, int x, int y, int width, int height, int fontSize){
+		button.setBounds(x,y,width,height);
+		button.setText(text);
+		button.setFont(new Font("Times New Roman", Font.PLAIN, fontSize));
+		
+	}
 
 }
