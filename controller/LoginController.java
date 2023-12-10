789493ec3451032559be8fbc12c0f56ab7bef52d
TheWhiteTorreta <torreta.markdwight@gmail.com>
2023-12-10 11:51:23 +0800

    Update to LoginController and AllView java file

--- controller/LoginController.java
+++ controller/LoginController.java
@@ -1,32 +1,47 @@
 package hatod.controller;
 
+import hatod.view.AllView;
 
-import javax.imageio.*;
-import java.io.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class LoginController{
 
-    Image backgroundImage;
     JTextField email;
-    JTextField name;
-    JTextField phone;
     JPasswordField password;
-    JTextField motorPlate;
+    JButton loginBtn;
+    JButton registerBtn;
+
+	JFrame frame;
 
     public LoginController(){
 
-        backgroundImage = Toolkit.getDefaultToolkit().createImage("LoginFrame.png");
+		frame = new JFrame();
+		frame.setSize(500,500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
 
-        
+        JLabel hatod = new JLabel();
+        AllView.label(hatod, frame, "HATOD", 300, 70, 150, 50, 40);
 
+        email = new JTextField();
+        AllView.textField(email, frame, "Enter email", 304, 193, 160, 25, 12);
 
+        password = new JPasswordField();
+        AllView.passField(password, frame, 304, 244, 160, 25);
+
+        loginBtn = new JButton();
+        AllView.button(loginBtn, frame, "LOGIN", 345, 301, 96, 25, 12);
+
+        registerBtn = new JButton();
+        AllView.button(registerBtn, frame, "REGISTER", 345, 338, 96, 25, 12);
+
+		frame.setVisible(true);
     }
 
-    public void paint(Graphics graphic){
-        graphic.drawImage(backgroundImage, 0, 0, null);
-    }
+    
 
 }
--- view/AllView.java
+++ view/AllView.java
@@ -5,32 +5,37 @@ import java.awt.*;
 
 public class AllView{
 
-    public static void label(JLabel label, JPanel panel, String text, int x, int y, int width, int fontSize){
+    public static void label(JLabel label, JFrame frame, String text, int x, int y, int width, int height, int fontSize){
         label.setBounds(x,y,width,height);
         label.setText(text);
         label.setFont(new Font("Times New Roman", Font.BOLD, fontSize));
-        panel.add(label);
+        frame.add(label);
     }
 
-    public static void textField(JTextField textField, JPanel panel, int x, int y, int width, int height, int fontSize){
+    public static void textField(JTextField textField, JFrame frame, String text, int x, int y, int width, int height, int fontSize){
         textField.setBounds(x,y,width,height);
+        textField.setText(text);
         textField.setFont(new Font("Times New Roman", Font.BOLD, fontSize));
-        panel.add(textField);
+        frame.add(textField);
     }
 
-    public static void passField(JPasswordField passField, JPanel panel, int x, int y, int width, int height){
+    public static void passField(JPasswordField passField, JFrame frame, int x, int y, int width, int height){
         passField.setBounds(x,y,width,height);
-        panel.add(passField);
+        frame.add(passField);
     }
 
-    public static void button(JButton button, JPanel panel, String text, int x, int y, int width, int height, int fontSize){
+    public static void button(JButton button, JFrame frame, String text, int x, int y, int width, int height, int fontSize){
         button.setBounds(x,y,width,height);
         button.setText(text);
         button.setFont(new Font("Times New Roman", Font.BOLD, fontSize));
-        panel.add(button);
+        frame.add(button);
     }
 
-    public static void addBackground()
+    public static void clearFrame(JFrame frame){
+		frame.getContentPane().removeAll();
+		frame.repaint();
+		frame.validate();
+	}
     
 
 }
