8e0ab0eb2e3ebde86af9028822d718aa31fa9928
Aaron Jake Alfante <151142188+aaaronjake@users.noreply.github.com>
2023-12-10 14:37:14 +0800

    Update LoginController.java

--- controller/LoginController.java
+++ controller/LoginController.java
@@ -1,47 +1,77 @@
 package hatod.controller;
 
-import hatod.view.AllView;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import hatod.view.*;
+import hatod.model.*;
 
-public class LoginController{
+public class LoginController implements ActionListener{
+	JLabel image;
+	JPanel panel;
+	JTextField usernameTField;
+	JPasswordField passTField;
+	JButton loginBtn;
+	JButton registerBtn;
+	JButton passengerBtn;
+	JButton driverBtn;
 
-    JTextField email;
-    JPasswordField password;
-    JButton loginBtn;
-    JButton registerBtn;
+	//JFrame frame = new JFrame();
+	JFrame fram = new JFrame();
+	public LoginController(JFrame frame){
 
-	JFrame frame;
+		fram = frame;
+        	panel = new JPanel();
+        	AllView.addPanel(frame, panel);
+      
+		image = new JLabel();
+		AllView.addImage(frame, image);
+			
+		JLabel usernameLabel = new JLabel();
+		AllView.addLabel(usernameLabel, panel, "USERNAME", 290,120,200,30,11);
 
-    public LoginController(){
+		usernameTField = new JTextField();
+		AllView.addTextField(usernameTField, panel, 290,140,185,30,12);
+		
+		JLabel passLabel = new JLabel();
+		AllView.addLabel(passLabel, panel, "PASSWORD", 290,180,200,30,11);
 
-		frame = new JFrame();
-		frame.setSize(500,500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(null);
-		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
+		passTField = new JPasswordField();
+		AllView.addPassField(passTField, panel, 290,200,185,30,12);
 
-        JLabel hatod = new JLabel();
-        AllView.label(hatod, frame, "HATOD", 300, 70, 150, 50, 40);
+		loginBtn = new JButton();
+		AllView.addButton(loginBtn, panel, "LOGIN", 310, 260, 150, 35, 12);
+		loginBtn.addActionListener(this);
 
-        email = new JTextField();
-        AllView.textField(email, frame, "Enter email", 304, 193, 160, 25, 12);
+		registerBtn = new JButton();
+		AllView.addButton(registerBtn, panel, "REGISTER", 310, 300, 150, 35, 12);
+		registerBtn.addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent event){
+		if(event.getSource()==loginBtn){
+			
+		}
+		if(event.getSource()==registerBtn){
+			driverBtn = new JButton();
+			passengerBtn = new JButton();
 
-        password = new JPasswordField();
-        AllView.passField(password, frame, 304, 244, 160, 25);
+			AllView.addButton(passengerBtn, "REGISTER AS PASSENGER", 292,360, 185, 35, 12);
+			passengerBtn.addActionListener(this);
+			panel.add(passengerBtn);
 
-        loginBtn = new JButton();
-        AllView.button(loginBtn, frame, "LOGIN", 345, 301, 96, 25, 12);
+			AllView.addButton(driverBtn, "REGISTER AS DRIVER", 292,400, 185, 35, 12);
+			panel.add(driverBtn);
 
-        registerBtn = new JButton();
-        AllView.button(registerBtn, frame, "REGISTER", 345, 338, 96, 25, 12);
-
-		frame.setVisible(true);
-    }
-
-    
+			panel.revalidate();
+			panel.repaint();
+		}
+		if(event.getSource()==passengerBtn){
+			fram.getContentPane().removeAll();
+			fram.validate();
+			fram.repaint();		
+			new RegisterController(fram);
+		}
+	}
 
 }
