7b2274a3563fc205cc09f78233d570760a4f3c69
Aaron Jake Alfante <151142188+aaaronjake@users.noreply.github.com>
2024-01-07 04:51:42 +0800

    Add files via upload

---
+++ controller/LoginController.java
@@ -0,0 +1,101 @@
+package hatod.controller;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import hatod.view.*;
+import hatod.model.*;
+
+public class LoginController implements ActionListener{
+	JLabel image;
+	JPanel panel;
+	
+	JTextField usernameTField;
+	JPasswordField passTField;
+	JButton loginBtn;
+	JButton registerBtn;
+	JButton passengerBtn;
+	JButton driverBtn;
+
+	//JFrame frame = new JFrame();
+	JFrame fram = new JFrame();
+	public LoginController(JFrame frame){
+
+		fram = frame;
+        panel = new JPanel();
+        AllView.addPanel(frame, panel);
+      
+		image = new JLabel();
+		AllView.addImage(frame, image, "1.png");
+			
+		JLabel usernameLabel = new JLabel();
+		AllView.addLabel(usernameLabel, panel, "USERNAME", 271,110,200,30,11);
+
+		usernameTField = new JTextField("USERNAME");
+		AllView.addTextField(usernameTField, panel, 271,130,185,30,12, "USERNAME");
+		
+		JLabel passLabel = new JLabel();
+		AllView.addLabel(passLabel, panel, "PASSWORD", 271,170,200,30,11);
+
+		passTField = new JPasswordField();
+		AllView.addPassField(passTField, panel, 271,190,185,30,12);
+
+		loginBtn = new JButton();
+		AllView.addButton(loginBtn, panel, "LOGIN", 291, 260, 150, 35, 12);
+		loginBtn.addActionListener(this);
+
+		registerBtn = new JButton();
+		AllView.addButton(registerBtn, panel, "REGISTER", 291, 300, 150, 35, 12);
+		registerBtn.addActionListener(this);
+
+	}
+	
+	public void actionPerformed(ActionEvent event){
+		if(event.getSource()==loginBtn){
+			String fullName = usernameTField.getText();
+			char[] passwordChars = passTField.getPassword();
+            String password = new String(passwordChars);
+
+           	LoginModel loginModel = new LoginModel();
+
+           	boolean loginSuccessful = loginModel.successfulLogin(fullName, password);
+
+           	if(loginSuccessful){
+
+           		AllView.removeFrame(fram);
+           		new UserController(fram);
+           		JOptionPane.showMessageDialog(panel, "REGISTERED SUCCESSFULLY");
+           	} else{
+           		JOptionPane.showMessageDialog(panel, "Registration Failed! Invalid input or username already exists");
+           	}
+		}
+		if(event.getSource()==registerBtn){
+			
+			passengerBtn = new JButton();
+			AllView.addButton(passengerBtn, "REGISTER AS PASSENGER", 271,360, 185, 35, 12);
+			passengerBtn.addActionListener(this);
+			panel.add(passengerBtn);
+			
+			driverBtn = new JButton();
+			AllView.addButton(driverBtn, "REGISTER AS DRIVER", 271,400, 185, 35, 12);
+			driverBtn.addActionListener(this);
+			panel.add(driverBtn);
+
+			panel.revalidate();
+			panel.repaint();
+		}
+		if(event.getSource()==passengerBtn){
+			AllView.removeFrame(fram);
+			
+			new RegisterController(fram);
+		}
+		if(event.getSource()==driverBtn){
+			AllView.removeFrame(fram);
+			
+			new DriverRegisterController(fram);
+		}
+
+
+	}
+
+}
