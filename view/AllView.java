9d897bb6db1ef4da90d9150947739b6ca0d67260
Aaron Jake Alfante <151142188+aaaronjake@users.noreply.github.com>
2024-01-08 04:04:07 +0800

    Update AllView.java

--- view/AllView.java
+++ view/AllView.java
@@ -12,21 +12,21 @@ public class AllView{
 	}
 	public static void addPanel(JFrame frame, JPanel panel){
 		panel.setBounds(0,0,500,500);
-        panel.setLayout(null);
-        panel.setOpaque(false);
-        frame.add(panel);
+        	panel.setLayout(null);
+        	panel.setOpaque(false);
+        	frame.add(panel);
 	}
 	public static void addLabel(JLabel label, JPanel panel, String text, int x, int y, int width, int height, int fontSize){
 		label.setBounds(x,y,width,height);
 		label.setText(text);
-		label.setFont(new Font("Times New Roman", Font.BOLD, fontSize));
-        label.setForeground(new Color(0xC9E7EB));
+		label.setFont(new Font("Inter", Font.BOLD, fontSize));
+        label.setForeground(new Color(0x000000));
 		panel.add(label);
 	}
 	public static void addLabel(JLabel label, JPanel panel, String text, int x, int y, int width, int height, int fontSize, int color){
 		label.setBounds(x,y,width,height);
 		label.setText(text);
-		label.setFont(new Font("Times New Roman", Font.BOLD, fontSize));
+		label.setFont(new Font("Inter", Font.BOLD, fontSize));
         label.setForeground(new Color(color));
 		panel.add(label);
 	}
@@ -41,6 +41,7 @@ public class AllView{
 	public static void addTextField(JTextField textField, JPanel panel, int x, int y, int width, int height, int fontSize, String text){
 		textField.setBounds(x,y,width,height);
 		textField.setFont(new Font("Times New Roman", Font.PLAIN, fontSize));
+		textField.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
 		//textField.setBackground(new Color(0x797E83));
 		
 		textField.addFocusListener(new FocusListener(){
@@ -53,68 +54,53 @@ public class AllView{
 					textField.setText(text);
 			}
 		});
-		
 		panel.add(textField);
 	}
 
 	public static void addTextField(JTextField textField, JPanel panel, int x, int y, int width, int height, int fontSize){
 		textField.setBounds(x,y,width,height);
 		textField.setFont(new Font("Times New Roman", Font.PLAIN, fontSize));
+		textField.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
 		//textField.setBackground(new Color(0x797E83));
-		
 		panel.add(textField);
 	}
 
 	public static void addPassField(JPasswordField passField, JPanel panel, int x, int y, int width, int height, int fontSize){
 		passField.setBounds(x,y,width,height);
 		passField.setFont(new Font("Times New Roman", Font.PLAIN, fontSize));
+		passField.setBorder(BorderFactory.createLineBorder(Color.RED));
 		passField.setToolTipText("Password must contain at least 8 characters");
-
-		//passField.setBackground(new Color(0x797E83));
 		passField.setBorder(BorderFactory.createLoweredBevelBorder());
-		
-		passField.addKeyListener(new KeyListener() {
- 
-		    @Override
-		    public void keyTyped(KeyEvent event) {
-		        // do something when a key has been typed
-		    }
-		 
-		    @Override
-		    public void keyReleased(KeyEvent event) {
-
-		        // do something when a key has been released
-		    }
-		 
-		    @Override
-		    public void keyPressed(KeyEvent event) {
-		        // do something when a key has been pressed
-		    }
-		});
 		panel.add(passField);
 	}
 
 	public static void addButton(JButton button, JPanel panel, String text, int x, int y, int width, int height, int fontSize){
 		button.setBounds(x,y,width,height);
 		button.setText(text);
-		button.setFont(new Font("Times New Roman", Font.BOLD, fontSize));
-		button.setBackground(new Color(0x797E83));
+		button.setFont(new Font("Inter", Font.BOLD, fontSize));
+		button.setBackground(new Color(0x503830));
 		button.setForeground(new Color(0x000000));
+		button.setFocusPainted(false);
+		button.setBorder(BorderFactory.createEmptyBorder());
 		panel.add(button);
 	}
 	public static void addButton(JButton button, JPanel panel, String text, int x, int y, int width, int height, int fontSize, int color){
 		button.setBounds(x,y,width,height);
 		button.setText(text);
-		button.setFont(new Font("Times New Roman", Font.BOLD, fontSize));
+		button.setFont(new Font("Inter", Font.BOLD, fontSize));
 		button.setBackground(new Color(color));
 		button.setForeground(new Color(0x000000));
+		button.setFocusPainted(false);
+		button.setBorder(BorderFactory.createEmptyBorder());
 		panel.add(button);
 	}
 	public static void addButton(JButton button, String text, int x, int y, int width, int height, int fontSize){
 		button.setBounds(x,y,width,height);
 		button.setText(text);
 		button.setFont(new Font("Times New Roman", Font.PLAIN, fontSize));
-		
+		button.setFocusPainted(false);
+		button.setBorder(BorderFactory.createEmptyBorder());
+
 	}
 
 	public static void removeFrame(JFrame frame){
