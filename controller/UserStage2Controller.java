7fa2205e34a08069491721b5acf6447bb6281c68
Aaron Jake Alfante <151142188+aaaronjake@users.noreply.github.com>
2024-01-08 03:56:56 +0800

    Add files via upload

---
+++ controller/UserStage2Controller.java
@@ -0,0 +1,34 @@
+package hatod.controller;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import hatod.view.*;
+import hatod.model.*;
+
+public class UserStage2Controller{
+	JLabel image;
+	JLabel costLabel;
+	JPanel panel;
+	JTextField pickUpField;
+	JTextField dropOffField;
+	JButton motorcycleBtn;
+	JButton taxiBtn;
+	JButton bikeBtn;
+	JButton bookBtn;
+
+	//JFrame frame = new JFrame();
+	JFrame fram = new JFrame();
+	public UserStage2Controller(JFrame frame){
+
+		fram = frame;
+        panel = new JPanel();
+        AllView.addPanel(frame, panel);
+      
+		image = new JLabel();
+		AllView.addImage(frame, image, "3.png");
+
+		JLabel onLook = new JLabel();
+		AllView.addLabel(onLook, panel, "LOOKING FOR RIDER...", 70, 395, 400, 40, 30, 0xF9732D);
+	}
+}
